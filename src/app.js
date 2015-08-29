16649c8da724a851084fd5fd577d5e60ccf81742
pabloalonso <pabloalonsos.eu@gmail.om>
2015-08-29 18:53:07 -0400

    Added Redux DevTools

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
 import Main from './containers/main-container';
 import Reducers from './reducers';
 
-let store = createStore(Reducers);
+const devCreateStore = compose(
+    devTools(),
+    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
+    createStore
+);
+
+let store = devCreateStore(Reducers);
 
 React.render(
-    <Provider store={store}>
-        {() => <Main />}
-    </Provider>,
+    <div>
+        <Provider store={store}>
+            {() => <Main />}
+        </Provider>
+        <DebugPanel top right bottom>
+            <DevTools store={store} monitor={LogMonitor} />
+        </DebugPanel>
+    </div>,
     document.getElementById('root')
 )
