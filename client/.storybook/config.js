d0912c885c0c02cadb36bbbca5c2708ba024a241
Adam Parrish <aparrish@neosavvy.com>
2016-04-06 22:19:12 -0400

    First storybook

---
+++ client/.storybook/config.js
@@ -0,0 +1,11 @@
+import { configure } from '@kadira/storybook';
+
+function loadStories() {
+    debugger;
+    const sample = require('../stories/sample');
+    debugger;
+    // require as many stories as you need.
+}
+
+debugger;
+configure(loadStories, module);
